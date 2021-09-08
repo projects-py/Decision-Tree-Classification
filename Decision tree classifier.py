39d137ff81bdd895f97847ca0394e9b4a3c3ed89
projects-py <73610847+projects-py@users.noreply.github.com>
2021-09-08 11:39:22 -0400

    Add files via upload

---
+++ Decision tree classifier.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#importing libraries
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+
+#loading data
+
+dataset = pd.read_csv("iphone_purchase_records.csv")
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 3].values
+
+#converting gender to a number
+
+labelEncoder_gender =  LabelEncoder()
+X[:,0] = labelEncoder_gender.fit_transform(X[:,0])
+
+
+
+X = np.vstack(X[:, :]).astype(np.float)
+
+#Splitting Data
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+#Fit the classifier
+
+classifier = DecisionTreeClassifier(criterion = "entropy", random_state=0)
+classifier.fit(X_train, y_train)
+
+#Prediction
+y_pred = classifier.predict(X_test)
+
+
+# Evaluate the model performance
+cm = metrics.confusion_matrix(y_test, y_pred) 
+print(cm)
+accuracy = metrics.accuracy_score(y_test, y_pred) 
+print("Accuracy score:",accuracy)
+precision = metrics.precision_score(y_test, y_pred) 
+print("Precision score:",precision)
+recall = metrics.recall_score(y_test, y_pred) 
+print("Recall score:",recall)
+
